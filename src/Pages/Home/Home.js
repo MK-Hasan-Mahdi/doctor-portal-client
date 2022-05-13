fcaab783e739f11fddcac4e541e3d46adb09d914
mk-hasan <mkhasan6891@gmail.com>
2022-05-13 15:59:58 +0600

    testimonials, review added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import OurCare from './OurCare';
 import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services />
             <OurCare></OurCare>
             <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
         </div>
     );
 };
