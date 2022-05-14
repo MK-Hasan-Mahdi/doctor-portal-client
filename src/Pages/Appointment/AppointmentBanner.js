09fc9ff9433e5eee77df467b14ac25a5da6ba6f4
mk-hasan <mkhasan6891@gmail.com>
2022-05-14 11:47:16 +0600

    background cover added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -2,11 +2,15 @@ import React from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import bg from '../../assets/images/bg.png';
 
 const AppointmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen">
+        <div style={{
+            background: `url(${bg})`,
+            backgroundSize: 'cover'
+        }} class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='chair' />
                 <div>
