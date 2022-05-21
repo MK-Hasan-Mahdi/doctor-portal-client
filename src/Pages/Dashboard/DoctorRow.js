9ae03793a42e12da0c68c4a6f7926054bb742c8e
mk-hasan <mkhasan6891@gmail.com>
2022-05-21 12:04:38 +0600

    delete api created in doctorrow

--- src/Pages/Dashboard/DoctorRow.js
+++ src/Pages/Dashboard/DoctorRow.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const DoctorRow = ({ doctor, index, refetch }) => {
-    const { name, speciality, img } = doctor;
+    const { name, speciality, img, email } = doctor;
+
+    const handleDelete = (email) => {
+        fetch(`http://localhost:5000/doctor/${email}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    toast.success(`Doctor ${name} deleted successfully`);
+                    refetch();
+                }
+            })
+    }
+
     return (
         <tr class="hover">
             <th>{index + 1}</th>
             <td><div class="avatar">
-                <div class="w-24  mask mask-squircle">
+                <div class="w-14  mask mask-squircle">
                     <img src={img} />
                 </div>
             </div>
             </td>
             <td>{name}</td>
             <td>{speciality}</td>
-            <td><button class="btn btn-outline btn-error">Delete</button></td>
+            <td><button onClick={() => handleDelete(email)} class="btn btn-outline btn-error">Delete</button></td>
         </tr>
     );
 };
